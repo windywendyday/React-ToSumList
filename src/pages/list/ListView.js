16a4828d9d74a98e7bf4ac519c657d56dbd3ae30
windywendyday <402089324@qq.com>
2024-08-12 00:33:16 +0800

    feat: 新建事项

--- src/pages/list/ListView.js
+++ src/pages/list/ListView.js
@@ -1,14 +1,21 @@
 import React, {useState} from "react";
 import UserInfo from "./components/userInfo/UserInfo.js";
 import ListContent from "./components/list/index.js";
-import {Form, Input, Popup} from "antd-mobile";
+import {Button, CalendarPicker, Form, Input, Popup, Stepper} from "antd-mobile";
 import {AddCircleOutline} from "antd-mobile-icons";
 import './ListView.css'
+import dayjs from "dayjs";
 
 export default function ListView() {
     const [popUpVisible, setPopUpVisible] = useState(false)
-    function createNewItem() {
+    const [datePickerVisible, setDatePickerVisible] = useState(false)
+    const singleDate= null
+    const [dateVal, setDateVal] = useState(new Date())
+    const [form] = Form.useForm()
 
+    function createNewItem() {
+        const formData = form.getFieldsValue()
+        console.log('formData',formData)
     }
     return (
         <div className='listView'>
@@ -19,17 +26,54 @@ export default function ListView() {
                 </div>
             </div>
             <ListContent></ListContent>
-            <Popup
-                visible={popUpVisible}
-                onMaskClick={() => setPopUpVisible(false)}
-                bodyStyle={{ height: '50vh' }}
-            >
-                <Form layout='horizontal'>
-                    <Form.Item label='事项名称' name='itemName'>
-                        <Input placeholder='请输入事项名' clearable />
-                    </Form.Item>
-                </Form>
-            </Popup>
+            <div className='popUp'>
+                <Popup
+                    visible={popUpVisible}
+                    onMaskClick={() => setPopUpVisible(false)}
+                    bodyStyle={{ minHeight: '50vh', borderTopLeftRadius: '8px', borderTopRightRadius: '8px', }}
+                >
+                    <Form layout='horizontal' form={form}>
+                        <Form.Header>新建事项</Form.Header>
+                        <Form.Item label='事项名称' name='itemName'>
+                            <Input placeholder='请输入事项名' clearable />
+                        </Form.Item>
+                        <Form.Item label='截止时间' name='itemDeadline'>
+                            <Input onClick={() => setDatePickerVisible(true)} name='dateVal' value={dayjs(dateVal).format('YYYY-MM-DD')}/>
+                            <div className='datePicker'>
+                                <CalendarPicker
+                                    visible={datePickerVisible}
+                                    selectionMode='single'
+                                    defaultValue={singleDate}
+                                    onClose={() => setDatePickerVisible(false)}
+                                    onMaskClick={() => setDatePickerVisible(false)}
+                                    onConfirm={val => {
+                                        setDateVal(val)
+                                        console.log('date:', dateVal)
+                                    }}
+                                />
+                            </div>
+                        </Form.Item>
+                        <Form.Item
+                            label='需打卡时间'
+                            name='needCheckInDays'
+                            childElementPosition='right'
+                            initialValue={1}
+                            rules={[
+                                {
+                                    max: 999,
+                                    min: 1,
+                                    type: 'number',
+                                },
+                            ]}
+                        >
+                            <Stepper />
+                        </Form.Item>
+                    </Form>
+                    <Button block color='primary' className='confirmButton' onClick={() => createNewItem()}>
+                        确认新建
+                    </Button>
+                </Popup>
+            </div>
         </div>
     )
 }
